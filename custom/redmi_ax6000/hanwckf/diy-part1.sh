31efbd1cc3fabb535910803fd198e92aef501e73
Weitian <31085999+weitian8@users.noreply.github.com>
2023-04-20 15:29:19 +0800

    Update diy-part1.sh

--- custom/redmi_ax6000/hanwckf/diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/diy-part1.sh
@@ -24,8 +24,8 @@
 # ./scripts/feeds install -a -f -p kenzo
 # ./scripts/feeds install -a -f -p small
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git luci https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git luci https://github.com/kenzok8/small' feeds.conf.default
 git pull
 ./scripts/feeds update -a
 ./scripts/feeds install -a
