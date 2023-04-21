f30250da7862c233d36cd672d8fe7b3d1e9cb6b2
Weitian <31085999+weitian8@users.noreply.github.com>
2023-04-21 11:13:27 +0800

    Update diy-part1.sh

--- custom/redmi_ax6000/hanwckf/diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/diy-part1.sh
@@ -18,12 +18,29 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # ./scripts/feeds update -a && ./scripts/feeds install -a
 # sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns' feeds.conf.default
-# ./scripts/feeds uninstall luci-app-passwall
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a -f -p kenzo
-# ./scripts/feeds install -a -f -p small
 
-sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+# ./scripts/feeds uninstall luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/chinadns-ng
+rm -rf feeds/luci/applications/dns2socks
+rm -rf feeds/luci/applications/dns2tcp
+rm -rf feeds/luci/applications/microsocks
+rm -rf feeds/luci/applications/shadowsocks-rust
+rm -rf feeds/luci/applications/shadowsocksr-libev
+rm -rf feeds/luci/applications/simple-obfs
+rm -rf feeds/luci/applications/tcping
+rm -rf feeds/luci/applications/trojan-go
+rm -rf feeds/luci/applications/trojan-plus
+rm -rf feeds/luci/applications/v2ray-core
+rm -rf feeds/luci/applications/v2ray-geodata
+rm -rf feeds/luci/applications/v2ray-plugin
+rm -rf feeds/luci/applications/xray-core
+rm -rf feeds/luci/applications/xray-plugin
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a -f -p kenzo
+./scripts/feeds install -a -f -p small
+
+# sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
